75536430e4daab4cec40955a6e01200d01dbda25
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-11-04 13:14:39 -0500

    fixed small error

--- projects/src/components/cards/CurProjects.js
+++ projects/src/components/cards/CurProjects.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const CurProjects = () => {
+
+const CurProjects = (props) => {
+    const { projects } = props
+    if (!projects || projects.length === 0) return <p>Adding Projects</p>
+    if (projects.extraSource === null) return <p></p>
     return (
         <>
         </>
